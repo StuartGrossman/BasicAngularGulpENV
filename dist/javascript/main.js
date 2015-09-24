ba9c3ad5d5dac97a11f303aa833a58cfdc52e19e
Stuart_Grossman <grossman.stuart1@gmail.com>
2015-09-23 17:04:47 -0700

    first commit

---
+++ dist/javascript/main.js
@@ -0,0 +1,39 @@
+(function() {
+	'use strict';
+	angular.module('app', ['ui.router'])
+	.config(Config);
+	Config.$inject = ['$stateProvider', '$urlRouterProvider'];
+	function Config($stateProvider, $urlRouterProvider) {
+		$stateProvider.state('Home',{
+			url: '/',
+			templateUrl: 'templates/Home.html'
+		});
+		$urlRouterProvider.otherwise('/');
+	}
+})();
+
+(function() {
+	'use strict';
+	angular.module('app')
+	.controller('HomeController', HomeController);
+
+	HomeController.$inject = [];
+
+	function HomeController() {
+		var vm = this;
+		vm.title = 'Welcome to our App!';
+	}
+})();
+(function() {
+	'use strict';
+	angular.module('app')
+	.factory('HomeFactory', HomeFactory);
+
+	HomeFactory.$inject = ['$http', '$q'];
+
+	function HomeFactory($http, $q) {
+		var o = {};
+		
+		return o;
+	}
+})();
